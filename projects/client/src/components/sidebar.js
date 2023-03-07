c8c30d0834d1e380e012bb019d128c9af7b31259
irsanrais <irsanrais94@gmail.com>
2023-03-07 22:46:59 +0700

    fix nama dashboard

--- projects/client/src/components/sidebar.js
+++ projects/client/src/components/sidebar.js
@@ -40,7 +40,7 @@ export default function Sidebar() {
         </ul>
       </div>
       <div className="h-screen flex-1 p-7">
-        <h1 className="text-2xl font-semibold ">Home Page</h1>
+        <h1 className="text-2xl font-semibold ">Dashboard Page</h1>
       </div>
     </div>
   );
