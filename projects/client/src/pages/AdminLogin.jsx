a1bc22078d86003c0ab67e311850866d640fb2b8
Selena Aura <obrolinae@gmail.com>
2023-03-09 15:28:54 +0700

    creating login feature for admin

--- projects/client/src/pages/AdminLogin.jsx
+++ projects/client/src/pages/AdminLogin.jsx
@@ -3,54 +3,59 @@ import React from "react";
 import { useState } from "react";
 import Axios from "axios";
 import { API_url } from "../helper";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {loginAction} from "../actions/adminsAction"
+import { loginAction } from "../actions/adminsAction";
 
 const AdminLogin = (props) => {
   const [show, setShow] = React.useState(false);
   const handleClick = () => setShow(!show);
 
   const [inputEmail, setInputEmail] = React.useState("");
-  const [inputPassword, setInputPassword] = React.useState("")
+  const [inputPassword, setInputPassword] = React.useState("");
 
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const loginButton = () => {
     Axios.post(API_url + "/admins/login", {
       email: inputEmail,
-      password: inputPassword
+      password: inputPassword,
     })
-    .then((response) => {
-      console.log("response:", response.data);
-      if(response.data.success){
-        dispatch(loginAction(response.data.value));
-        localStorage.setItem("admin_login", response.data.value.token);
-        navigate("/", {replace: true});
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
+      .then((response) => {
+        console.log("response:", response.data);
+        if (response.data.success) {
+          dispatch(loginAction(response.data.value));
+          localStorage.setItem("admin_login", response.data.value.token);
+          navigate("/", { replace: true });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <div>
-        <Box w={[300, 400, 500]}>
-          <Text fontSize="lg">e-mail</Text>
-          <Input placeholder="your.email@mail.com" />
-          <Text fontSize="lg">Password</Text>
-          <InputGroup size="md">
-            <Input pr="4.5rem" type={show ? "text" : "password"} placeholder="Enter password" />
-            <InputRightElement width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? "Hide" : "Show"}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-          <Button colorScheme="blackAlpha" onClick={loginButton}>Log in</Button>
-        </Box>
+    <div className="flex justify-center items-center h-screen">
+      <Box w={[300, 400, 500]} className="shadow p-5">
+      <Text className="font-bold" fontSize="4xl">
+          Login as admin
+        </Text>
+        <Text className="mt-3" fontSize="lg">e-mail</Text>
+        <Input placeholder="Enter your.email@mail.com" />
+        <Text fontSize="lg">Password</Text>
+        <InputGroup size="md">
+          <Input pr="4.5rem" type={show ? "text" : "password"} placeholder="Enter password" />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Button className="mt-5" colorScheme="twitter" onClick={loginButton}>
+          Login
+        </Button>
+      </Box>
     </div>
   );
 };
