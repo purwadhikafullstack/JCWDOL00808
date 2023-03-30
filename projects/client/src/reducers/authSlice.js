5a57feb9ab9de6a106f70c4aebb9561d525b6178
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-03-30 15:05:05 +0700

    create login reducer

--- projects/client/src/reducers/authSlice.js
+++ projects/client/src/reducers/authSlice.js
@@ -1,38 +1,35 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export const userLogin = createAsyncThunk(
   "auth/userLogin",
-  async ({ email, password }, { rejectWithValue }) => {
+  async ({ email, password }, thunkAPI) => {
     try {
       const response = await axios.post(
         `${process.env.REACT_APP_API_BASE_URL}/user/login`,
-        {
-          email,
-          password,
-        }
+        { email, password }
       );
-      localStorage.setItem("token", response?.data?.data?.token);
-      return response?.data?.data;
+      localStorage.setItem("token", response.data.data.token);
+      return response?.data;
     } catch (error) {
-      if (error.response && error.response.data.message) {
-        rejectWithValue(error.response.data.message);
+      if (error.response && error.response?.data?.message) {
+        return thunkAPI.rejectWithValue(error.response?.data?.message);
       } else {
-        rejectWithValue(error.message);
+        return thunkAPI.rejectWithValue(error.message);
       }
     }
   }
 );
 
-const token = localStorage.getItem("token")
-  ? localStorage.getItem("token")
-  : null;
+const token = localStorage.getItem("token") || null;
 
 const initialState = {
   isLoading: false,
   isSuccess: false,
   isError: false,
-  user: null,
+  message: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   token,
 };
 
@@ -40,29 +37,46 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    logout: (state) => {
-      localStorage.removeItem("token");
-      state.isLoading = false;
+    userLogout: (state) => {
       state.user = null;
       state.token = null;
-      state.isError = null;
+      state.message = null;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.isError = false;
+      localStorage.clear();
+      setTimeout(() => {
+        toast.success("Account logged out.");
+      }, 500);
     },
-    extraReducers: {
-      [userLogin.pending]: (state) => {
-        state.isLoading = true;
-      },
-      [userLogin.fulfilled]: (state, action) => {
-        state.isLoading = false;
-        state.isSuccess = true;
-        state.user = action.payload;
-        state.token = action.payload.token;
-      },
-      [userLogin.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.isError = action.payload;
-      },
+    clearState: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = null;
+    },
+  },
+  extraReducers: {
+    [userLogin.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.isError = false;
+      state.token = action.payload.data.token;
+      state.user = action.payload.data.user;
+      state.message = action.payload.message;
+      toast.success(state.message);
+    },
+    [userLogin.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+      toast.error(state.message);
+    },
+    [userLogin.pending]: (state) => {
+      state.isLoading = true;
     },
   },
 });
 
+export const { userLogout, clearState } = authSlice.actions;
 export default authSlice.reducer;
--- projects/client/src/reducers/index.js
+++ projects/client/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { adminsReducer } from "./adminsReducer";
-import { warehousesReducer } from "./warehousesReducer";
-import cartReducer from "./cartSlice";
 import authReducer from "./authSlice";
+import cartReducer from "./cartSlice";
+import { warehousesReducer } from "./warehousesReducer";
 
 export const globalStore = configureStore({
   reducer: {
