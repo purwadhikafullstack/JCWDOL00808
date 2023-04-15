53571911636f8b82facec386f0d761b419711025
ajikurnia0 <ajikurniarahman@gmail.com>
2023-04-15 09:33:26 +0700

    refactoring-ui login

--- projects/client/src/pages/user/Login.jsx
+++ projects/client/src/pages/user/Login.jsx
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link as RouterLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogin, clearState } from "../../reducers/authSlice";
-import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const { user, isSuccess } = useSelector((state) => state.auth);
+  const [isLoading, setIsLoading] = useState(false);
+  const { user, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+  const toast = useToast();
 
   const [loginData, setLoginData] = useState({
     email: "",
@@ -31,102 +52,119 @@ const Login = () => {
         navigate("/");
         dispatch(clearState());
       }, 2000);
+      toast({
+        title: "Logged in successfully.",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+    } else if (isError) {
+      toast({
+        title: message || "Login failed.",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+      dispatch(clearState());
     } else if (user) {
       navigate("/");
     }
-  }, [user, isSuccess, navigate, dispatch]);
+  }, [user, isSuccess, isError, message, navigate, dispatch, toast]);
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(6, "Must be at least 6 characters")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      dispatch(userLogin(values));
+    },
+  });
 
   return (
     <div className="flex justify-start items-center flex-col h-screen w-full">
-      <div className="relative w-full h-full">
-        <video
-          // src={shareVideo}
-          data-type="video/mp4"
-          loop
-          controls={false}
-          muted
-          autoPlay
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg bg-blackOverlay">
-          <div className="w-[396px] mx-auto h-screen">
-            <div className="border my-8 pb-6 bg-white border-gray-300">
-              <h1 className="text-center text-3xl my-4 py-6">My Store</h1>
-              <form className="flex flex-col gap-4">
-                <input
-                  placeholder="Email"
-                  className="bg-gray-50 mx-8 py-1 placeholder:text-xs focus:outline-1 focus:outline-gray-400 placeholder:text-gray-400 border rounded-sm p-2"
-                  type="text"
-                  name="email"
-                  onChange={changeHandler}
-                  required
-                />
-                <input
-                  id="password"
-                  placeholder="Password"
-                  className="bg-gray-50 mx-8 py-1 placeholder:text-xs focus:outline-1 focus:outline-gray-400 placeholder:text-gray-400 border rounded-sm p-2"
-                  type={passwordShown ? "text" : "password"}
-                  name="password"
-                  onChange={changeHandler}
-                  required
-                />
-                <div className="mx-auto">
-                  <input
-                    className="mr-1"
-                    id="toggle"
-                    name="toggle"
-                    type="checkbox"
-                    onClick={togglePasswordVisiblity}
+      <Flex
+        minH={"100vh"}
+        w={"full"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}>
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
+              Login
+            </Heading>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}>
+            <Stack spacing={4}>
+              <form onSubmit={formik.handleSubmit}>
+                <FormControl
+                  id="email"
+                  isRequired
+                  isInvalid={formik.touched.email && formik.errors.email}>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
                   />
-                  Show Password
-                </div>
+                  <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                </FormControl>
 
-                <button
-                  className="bg-sky-500 text-center mx-8 text-sm py-1 rounded-sm text-white font-bold"
-                  type="button"
-                  onClick={() => {
-                    // loginUser(loginData.email, loginData.password);
-                    dispatch(
-                      userLogin({
-                        email: loginData.email,
-                        password: loginData.password,
-                      })
-                    );
-                  }}
-                >
-                  Log in
-                </button>
+                <FormControl
+                  id="password"
+                  isRequired
+                  isInvalid={formik.touched.password && formik.errors.password}>
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                  />
+                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                </FormControl>
+
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    isLoading={isLoading}
+                    type="submit"
+                    loadingText="Submitting"
+                    size="md"
+                    bg={"blue.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "blue.500",
+                    }}>
+                    Sign up
+                  </Button>
+                </Stack>
+                <Stack pt={6}>
+                  <Text align={"center"}>
+                    Don't have an account?{" "}
+                    <Link as={RouterLink} to="/user/login" color={"blue.400"}>
+                      Sign up
+                    </Link>
+                  </Text>
+                </Stack>
               </form>
-
-              <h1
-                className="text-center mt-12 text-xs text-indigo-900 cursor-pointer"
-                onClick={() => navigate("/user/reset-password")}
-              >
-                Forgot password?
-              </h1>
-            </div>
-            <div className="border bg-white text-center py-4 border-gray-300 ">
-              <h1 className="text-sm">
-                Don't have an account?{" "}
-                <span
-                  onClick={() => navigate("/user/register")}
-                  className="text-sky-500 font-bold cursor-pointer"
-                >
-                  Sign up
-                </span>
-              </h1>
-            </div>
-            <div>
-              {/* <h1 className="text-center py-4 text-sm">Get the app.</h1> */}
-              <div className="flex scale-50 gap-6 justify-evenly">
-                {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/320px-Download_on_the_App_Store_Badge.svg.png" />{" "}
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/320px-Google_Play_Store_badge_EN.svg.png" /> */}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Toaster />
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
     </div>
   );
 };
