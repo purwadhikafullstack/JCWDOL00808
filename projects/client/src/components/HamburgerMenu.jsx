7fbbae557d82398e887903cf5785640c4d0b6a91
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-05-03 16:35:22 +0700

    add transaction and change font

--- projects/client/src/components/HamburgerMenu.jsx
+++ projects/client/src/components/HamburgerMenu.jsx
@@ -32,29 +32,35 @@ function HamburgerMenuButton(props) {
           {props.profile ? (
             <div className="absolute top-1 right-0 z-50 w-48 mt-12 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="px-4 py-3">
-                <p className="text-sm font-medium text-gray-900">
+                <p className="text-sm font-medium text-gray-900 font-[Oswald]">
                   Signed in as
                 </p>
-                <p className="text-sm font-medium text-gray-900">
+                <p className="text-sm font-medium text-gray-900 font-[Oswald]">
                   {props.profile.email}
                 </p>
               </div>
               <div className="py-1">
                 <Link
                   to="/user/profile"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-[Roboto]"
                 >
                   Edit Profile
                 </Link>
                 <Link
                   to="/user/address"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-[Roboto]"
                 >
                   Edit Address
                 </Link>
+                <Link
+                  to="/user/order-list"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-[Roboto]"
+                >
+                  My Transaction
+                </Link>
                 <span
                   onClick={() => logout(navigate, dispatch)}
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-[Roboto]"
                 >
                   Logout
                 </span>
@@ -65,7 +71,7 @@ function HamburgerMenuButton(props) {
               <div className="px-4 py-1">
                 <Link
                   to="/user/login"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-[Roboto]"
                 >
                   Sign in as user
                 </Link>
@@ -73,7 +79,7 @@ function HamburgerMenuButton(props) {
               <div className="py-1">
                 <Link
                   to="/admin/login"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 font-[Roboto]"
                 >
                   Sign in as admin
                 </Link>
