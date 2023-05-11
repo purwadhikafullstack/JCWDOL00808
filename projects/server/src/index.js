af30f2327e2d223b78e6825d6fda8606b5dc772c
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2023-05-11 21:11:26 +0700

    Update index.js

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,8 +1,7 @@
-const dotenv = require("dotenv");
-dotenv.config();
+const { join } = require("path");
+require('dotenv').config({path:join(__dirname,'../.env')});
 const express = require("express");
 const cors = require("cors");
-const { join } = require("path");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
