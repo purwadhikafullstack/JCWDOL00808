8310ce3877c0e5d043258a3ce9be0a1a4f394e13
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-05-11 17:30:52 +0700

    add optional chaining to prevent error

--- projects/server/src/helper/deleteFiles.js
+++ projects/server/src/helper/deleteFiles.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const deleteFiles = (files) => {
-  files.forEach((value) => {
+  files?.forEach((value) => {
     fs.unlink(value.path, function (err) {
       try {
         if (err) throw err;
